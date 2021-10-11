483cae9470a9f7e1fd75a0d64040c20750ababf9
Luis Ramos <feram99ag@gmail.com>
2021-10-11 00:25:03 -0400

    ops: create install & update shell scripts

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+# Description: Update F1-LED-Leaderboard software (github.com/feram18/f1-led-leaderboard)
+
+# Cleans up repository directory
+function clean() {
+  rm -f "*.log*"  # Log files
+  sudo rm -rf "*/__pycache__"  # pycache
+  sudo rm -rf "__pycache__"
+}
+
+# Updates repository
+function updateRepository() {
+  printf "Updating repository...\n"
+  git reset --hard
+  git checkout master
+  git fetch origin --prune
+  git pull
+}
+
+# Installs dependencies
+function installDependencies(){
+  printf "\nInstalling dependencies...\n"
+  sudo pip3 install -r requirements.txt
+}
+
+function main() {
+  clean
+  updateRepository
+  installDependencies
+
+  # Allow scripts to be easily executed next time
+  chmod +x install.sh update.sh
+
+  echo "$(tput setaf 2)Update completed$(tput setaf 7)"
+}
+
+# Execute script
+main
