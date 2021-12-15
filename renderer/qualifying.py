f231e51e512ed9c955a8c6c65f3c5c735ef134d0
Luis Ramos <feram99ag@gmail.com>
2021-12-15 14:00:37 -0500

    fix: render qualifying status only when there's an upcoming GP

--- renderer/qualifying.py
+++ renderer/qualifying.py
@@ -25,9 +25,7 @@ class Qualifying(Renderer):
         super().__init__(matrix, canvas, config)
         self.data = data
         self.qualifying = self.data.qualifying
-
         self.coords = self.config.layout.coords['qualifying']
-
         self.offset = 4
         self.position_x = self.coords['grid']['odd']['position']['x']
         self.position_y = self.coords['grid']['odd']['position']['y']
@@ -35,26 +33,27 @@ class Qualifying(Renderer):
         self.code_y = self.coords['grid']['odd']['code']['y']
 
     def render(self):
-        self.canvas.Clear()
+        if self.data.next_gp:
+            self.canvas.Clear()
 
-        if self.qualifying is None:
-            self.render_header()
-            x, y = align_text('Upcoming',
-                              Position.CENTER,
-                              Position.CENTER,
-                              self.canvas.width,
-                              self.canvas.height,
-                              self.font.baseline - 1,
-                              self.font.height)
-            DrawText(self.canvas, self.font, x, y, Color.WHITE.value, 'Upcoming')
-            time.sleep(7.0)
-        else:
-            # Render grid
-            pages = split_into_pages(self.qualifying.grid, 7)  # 1-7, 8-14, 15-20
-            for page in pages:
-                self.render_page(page)
+            if not self.qualifying:
+                self.render_header()
+                x, y = align_text('Upcoming',
+                                  Position.CENTER,
+                                  Position.CENTER,
+                                  self.canvas.width,
+                                  self.canvas.height,
+                                  self.font.baseline - 1,
+                                  self.font.height)
+                DrawText(self.canvas, self.font, x, y, Color.WHITE.value, 'Upcoming')
+                time.sleep(7.0)
+            else:
+                # Render grid
+                pages = split_into_pages(self.qualifying.grid, 7)  # 1-7, 8-14, 15-20
+                for page in pages:
+                    self.render_page(page)
 
-        self.canvas = self.matrix.SwapOnVSync(self.canvas)
+            self.canvas = self.matrix.SwapOnVSync(self.canvas)
 
     def render_header(self):
         header_x = align_text('Qualifying',
